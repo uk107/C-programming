5b891ea3d4816b78b0afd32223a896ef16a7292c
minseokK <alstjr133@naver.com>
2024-07-23 13:19:21 +0900

    2024/07/23 update

--- Functions_Flow_Control/minseokK/Flow_Control/1.c
+++ Functions_Flow_Control/minseokK/Flow_Control/1.c
@@ -1,7 +1,17 @@
 //
 // Created by MinseokK on 24. 7. 23.
 //
+// Check if a Number is Even or Odd
+#include <stdio.h>
 int main(){
-    printf("hello");
+    int number;
+    printf("input number : ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) {
+        printf("this number is Even");
+    }else {
+        printf("this number is Odd");
+    }
+
 }
-//minseokK
