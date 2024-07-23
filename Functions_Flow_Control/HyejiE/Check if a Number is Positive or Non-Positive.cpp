5a88a7c139184d3647448d4ae452c6a5ce377c42
HaejiK <hjlove001@gmail.com>
2024-07-23 14:08:07 +0900

    feat : Check if a Number is Positive or Non-Positive

---
+++ Functions_Flow_Control/HyejiE/Check if a Number is Positive or Non-Positive.cpp
@@ -0,0 +1,12 @@
+//
+// Created by HaejiK on 2024-07-23.
+//
+# include <stdio.h>
+int main() {
+    int number;
+    printf("Enter the number");
+    scanf("%d", &number);
+    if (number > 0) {
+        printf("Number is positive");
+    } else printf("Number is Non-positive");
+}
