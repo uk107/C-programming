7aafe17f4dc04758b369403cc12c6b52473dafad
minseokK <alstjr133@naver.com>
2024-07-24 15:05:26 +0900

    2024/07/24

--- Functions_Flow_Control/minseokK/Switch_Case/3.c
+++ Functions_Flow_Control/minseokK/Switch_Case/3.c
@@ -1,3 +1,43 @@
 //
 // Created by MinseokK on 24. 7. 23.
 //
+#include <stdio.h>
+
+void hi_day(int day) {
+    switch (day) {
+        case 1:
+            printf("happy Mon\n");
+        break;
+        case 2:
+            printf("Happy Tuesday.\n");
+        break;
+        case 3:
+            printf("Wonderful Wednesday\n");
+        break;
+        case 4:
+            printf("good Thursday!\n");
+        break;
+        case 5:
+            printf("Fantastic Friday!\n");
+        break;
+        case 6:
+            printf("Superb Saturday!\n");
+        break;
+        case 7:
+            printf("nice Sunday!\n");
+        break;
+        default:
+            printf("enter day\n");
+    }
+}
+
+int main() {
+    int day;
+
+    printf("enter your day : ");
+    scanf("%d", &day);
+
+    hi_day(day);
+
+    return 0;
+}
