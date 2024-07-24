c3562ed038a82edbbcef9979963ad9d304193d1a
seongukk <64528426+uk107@users.noreply.github.com>
2024-07-24 17:09:18 +0900

    change

--- Functions_Flow_Control/seongkk/Functions/Function_Assignment.cpp
+++ Functions_Flow_Control/seongkk/Functions/Function_Assignment.cpp
@@ -1,3 +1,115 @@
+// //1. Function to Calculate the Area of a Circle (TSRS)
+// #include <stdio.h>
 //
-// Created by SeongukK on 24/07/2024.
 //
+// double calculateCircle(double radius) {
+//     const double pi = 3.141592;
+//     return pi * radius * radius;
+// }
+//
+// int main() {
+//     double radius;
+//
+//     printf("wirte radius : ");
+//     scanf("%lf", &radius);
+//
+//
+//     float area = calculateCircle(radius);
+//
+//
+//     printf("radius is %.2lf area is %.2lf", radius, area);
+//
+ //     return 0;
+// }
+//
+
+
+//2. Function to Calculate Simple Interest (TSRS)
+
+// #include <stdio.h>
+//
+// float calculateSimpleInterest(float principal, float rate, float time) {
+//     return (principal * rate * time) / 100;
+// }
+//
+// int main() {
+//     float principal, rate, time, interest;
+//
+//
+//     printf("principal: ");
+//     scanf("%f", &principal);
+//
+//     printf("rate: ");
+//     scanf("%f", &rate);
+//
+//     printf("time: ");
+//     scanf("%f", &time);
+//
+//
+//     interest = calculateSimpleInterest(principal, rate, time);
+//
+//
+//     printf("The simple interest is: %.2f\n", interest);
+//
+//     return 0;
+// }
+
+// //3. Function to Check if a Number is Even or Odd (TSRS)
+
+// #include <stdio.h>
+//
+// void EvenOrOdd(int number) {
+//     if (number % 2 == 0) {
+//         printf("Even number", number);
+//     } else {
+//         printf("odd number", number);
+//     }
+// }
+//
+// int main() {
+//     int number;
+//
+//
+//     printf("Enter a number: ");
+//     scanf("%d", &number);
+//
+//
+//     EvenOrOdd(number);
+//
+//     return 0;
+// }
+
+
+
+// //4. Function to Print the First N Natural Numbers (TSRN)
+
+// #include <stdio.h>
+//
+//
+// void NaturalNumbers(int N) {
+//     for (int i = 1; i <= N; i++) {
+//         printf("%d ", i);
+//     }
+//     printf(" ");
+// }
+//
+// int main() {
+//     int N;
+//
+//
+//     printf("write a number(N) : ");
+//     scanf("%d", &N);
+//
+//
+//     if (N <= 0) {
+//         printf("error");
+//     } else {
+//
+//        NaturalNumbers(N);
+//     }
+//
+//     return 0;
+// }
+
+
+// //5. Function to Print the First N Odd Natural Numbers
--- Operators/seongukk/Operator_Assignment.cpp
+++ Operators/seongukk/Operator_Assignment.cpp
@@ -1,3 +1,179 @@
+// 10. Calculate Area and Perimeter of a Rectangle
 //
-// Created by SeongukK on 24/07/2024.
+// #include <stdio.h>
 //
+// int RectangleArea(int base, int height) {
+//     return base * height;
+// }
+//
+// int RectanglePerimeter(int base, int height) {
+//     return (base + height) * 2;
+// }
+//
+// int main() {
+//     int area, perimeter, base, height;
+//
+//
+//     printf("write base and height : ");
+//     scanf("%d %d", &base, &height);
+//
+//
+//     area = RectangleArea(base, height);
+//     perimeter = RectanglePerimeter(base, height);
+//
+//
+//     printf("area is : %d and perimeter is %d\n", area, perimeter);
+//
+//     return 0;
+// }
+
+// 11 .Append a Digit to a Number
+// #include <stdio.h>
+//
+// int main() {
+//
+//     int FstInt, ApndNum, result;
+//
+//     printf("write a First Integer : ");
+//     scanf(" %d", &FstInt);
+//
+//     printf("Enter the number you want to append : ");
+//     scanf(" %d", &ApndNum);
+//
+//     result = FstInt * 10 + ApndNum;
+//
+//     printf("result : %d", result);
+//
+//
+//     return 0;
+//
+// }
+
+// 12 .Add Two Complex Numbers
+
+// #include <stdio.h>
+//
+//
+// typedef struct {
+//     double real;
+//     double imag;
+// } Complex;
+//
+// void printComplex(Complex c) {
+//     if (c.imag >= 0)
+//         printf("%.2f + %.2fi\n", c.real, c.imag);
+//     else
+//         printf("%.2f - %.2fi\n", c.real, -c.imag);
+// }
+//
+// Complex addComplex(Complex c1, Complex c2) {
+//     Complex result;
+//     result.real = c1.real + c2.real;
+//     result.imag = c1.imag + c2.imag;
+//     return result;
+// }
+
+// 13 .Calculate Simple Interest#include <stdio.h>
+// #include <stdio.h>
+//
+// double calculateSimpleInterest(double principal, double rate, double time) {
+//     return (principal * rate * time) / 100.0;
+// }
+//
+// int main() {
+//     double principal, rate, time, interest;
+//
+//
+//     printf("Enter the principal amount: ");
+//     scanf("%lf", &principal);
+//
+//     printf("Enter the rate of interest: ");
+//     scanf("%lf", &rate);
+//
+//     printf("Enter the time period in years: ");
+//     scanf("%lf", &time);
+//
+//
+//     interest = calculateSimpleInterest(principal, rate, time);
+//
+//
+//     printf("Simple Interest: %.2f\n", interest);
+//
+//     return 0;
+// }
+
+
+// 14 .Calculate Compound Interest
+
+// #include <stdio.h>
+//
+//
+// double power(double base, int exponent) {
+//     double result = 1.0;
+//     for (int i = 0; i < exponent; ++i) {
+//         result *= base;
+//     }
+//     return result;
+// }
+//
+//
+// double calculateCompoundInterest(double principal, double rate, int times, double time) {
+//     double amount;
+//     double factor = 1 + rate / (100.0 * times);
+//     amount = principal * power(factor, times * time);
+//     return amount;
+// }
+//
+// int main() {
+//     double principal, rate, time, amount;
+//     int times;
+//
+//
+//     printf("Enter the principal amount: ");
+//     scanf("%lf", &principal);
+//
+//     printf("Enter the annual rate of interest: ");
+//     scanf("%lf", &rate);
+//
+//     printf("Enter the number of times interest is compounded per year: ");
+//     scanf("%d", &times);
+//
+//     printf("Enter the time period in years: ");
+//     scanf("%lf", &time);
+//
+//
+//     amount = calculateCompoundInterest(principal, rate, times, time);
+//
+//
+//     printf("Compound Interest Amount: %.2f\n", amount);
+//
+//     return 0;
+// }
+//
+
+
+// 15. Check If a Number is Even or Odd Using Bitwise Operator
+
+// #include <stdio.h>
+//
+//
+// void EvenOrOdd(int number) {
+//     if (number & 1) {
+//         printf("The number %d is Odd.\n", number);
+//     } else {
+//         printf("The number %d is Even.\n", number);
+//     }
+// }
+//
+// int main() {
+//     int number;
+//
+//
+//     printf("wirte a number: ");
+//     scanf("%d", &number);
+//
+//
+//     EvenOrOdd(number);
+//
+//     return 0;
+// }
